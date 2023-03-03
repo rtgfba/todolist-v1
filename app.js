0f01dab095ce0f323b6421402c27462b64bc501b
rtg <120173063+rtgfba@user.nonreply.github.com>
2023-03-02 23:46:01 -0500

    Create todolist server

---
+++ app.js
@@ -0,0 +1,45 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+
+let items = ["Buy food", "Cook food", "Eat food",];
+
+
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({extended:true}));
+
+app.use(express.static("public"));
+ 
+app.get("/", function(req,res){
+    //write logic code and send the result back to the browser 
+
+    let today = new Date();
+    let options = {
+        weekday: "long",
+        day: "numeric",
+        month: "long",
+    };
+
+    let day = today.toLocaleDateString("en-US", options );
+    
+
+    
+    res.render("list", {kindOfDay: day, newListItems: items}); 
+    });
+
+
+app.post("/", function(req,res){
+    var item = req.body.newItem
+
+   items.push(item);
+
+   res.redirect("/");
+});
+
+
+
+app.listen(3000, function(){
+    console.log("Server started on port 3000")
+});
