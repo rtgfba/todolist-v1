198b8da811efed8817635dc37424d05e50109c3d
rtg <120173063+rtgfba@user.nonreply.github.com>
2023-03-03 23:25:38 -0500

    refactor code

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const date = require(__dirname +"/date.js");
 
 const app = express();
 
-let items = ["Buy food", "Cook food", "Eat food",];
-let workItems = []
+
+
+
+const items = ["Buy food", "Cook food", "Eat food",];
+const workItems = []
 
 
 app.set('view engine', 'ejs');
@@ -15,17 +19,7 @@ app.use(express.static("public"));
  
 app.get("/", function(req,res){
     //write logic code and send the result back to the browser 
-
-    let today = new Date();
-    let options = {
-        weekday: "long",
-        day: "numeric",
-        month: "long",
-    };
-
-    let day = today.toLocaleDateString("en-US", options );
-    
-
+    const day = date.getDate();
     
     res.render("list", {listTitle: day, newListItems: items}); 
     });
@@ -33,7 +27,7 @@ app.get("/", function(req,res){
 
 app.post("/", function(req,res){
 
-    let item = req.body.newItem
+    const item = req.body.newItem
 
     if(req.body.list === "Work"){
         workItems.push(item);
@@ -54,7 +48,7 @@ app.get("/work",function(req,res){
 });
 
 app.post("/work", function(req,res){
-    let item = req.body.newItem;
+    const item = req.body.newItem;
     workItems.push(item);
     res.redirect("/work")
 });
