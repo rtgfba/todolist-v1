20c6acfe29f476959badf00bd957f4893bf926a1
rtg <120173063+rtgfba@user.nonreply.github.com>
2023-03-03 20:55:45 -0500

    create logic to server in order to post list.ejs

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const bodyParser = require("body-parser");
 const app = express();
 
 let items = ["Buy food", "Cook food", "Eat food",];
+let workItems = []
 
 
 app.set('view engine', 'ejs');
@@ -26,19 +27,37 @@ app.get("/", function(req,res){
     
 
     
-    res.render("list", {kindOfDay: day, newListItems: items}); 
+    res.render("list", {listTitle: day, newListItems: items}); 
     });
 
 
 app.post("/", function(req,res){
-    var item = req.body.newItem
 
-   items.push(item);
+    let item = req.body.newItem
 
-   res.redirect("/");
+    if(req.body.list === "Work"){
+        workItems.push(item);
+        res.redirect("/Work");
+    }else{
+        items.push(item);
+        res.redirect("/");
+    }
+
+  
+
+  
 });
 
 
+app.get("/work",function(req,res){
+    res.render("list", {listTitle: "Work List", newListItems: workItems});
+});
+
+app.post("/work", function(req,res){
+    let item = req.body.newItem;
+    workItems.push(item);
+    res.redirect("/work")
+});
 
 app.listen(3000, function(){
     console.log("Server started on port 3000")
